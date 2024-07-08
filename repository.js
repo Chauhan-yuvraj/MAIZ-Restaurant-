b07f1f171dcc1f4b96e6cfed9d2a9b6ff27f0c02
CHAUHAN YUVRAJ SINGH <113570060+Chauhan-yuvraj@users.noreply.github.com>
2024-07-08 17:48:26 +0530

    Create repository.js

---
+++ repository.js
@@ -0,0 +1,9 @@
+{
+  "name": "MAIZ : An application for international Restaurant",
+  "description": "MAIZ: A comprehensive app for international restaurants, offering easy reservations, authentic cuisine discovery, user reviews, and exclusive deals, ensuring an enriched global dining experience for food enthusiasts everywhere.",
+  "year": "2024",
+  "role": "front-end developer",
+  "avatar": "MAIZ.png",
+  "demo": "https://github.com/Chauhan-yuvraj/MAIZ-Restaurant-",
+  "github": "https://github.com/Chauhan-yuvraj/MAIZ-Restaurant-"
+}
